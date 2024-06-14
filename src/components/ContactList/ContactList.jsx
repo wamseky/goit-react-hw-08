1c92757cac5fa943702cce318f64603cb2cf24f3
Diana Kharchenko <vitadiana2@gmail.com>
2024-06-14 19:02:07 +0300

    .

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,19 +1,21 @@
 import { useSelector } from 'react-redux';
 import { useAutoAnimate } from '@formkit/auto-animate/react';
-import { selectNameFilter } from '../../redux/filters/selectors';
+// import { selectNameFilter } from '../../redux/filters/selectors';
+import { selectFilteredContacts } from '../../redux/contacts/selectors'
 import Contact from '../Contact/Contact';
 import css from './ContactList.module.css';
 
-const selectContacts = state => state.contacts.items;
+// const selectContacts = state => state.contacts.items;
 
 export default function ContactList() {
   const [parent] = useAutoAnimate({ easing: 'linear', duration: 300 });
-  const contacts = useSelector(selectContacts);
-  const filter = useSelector(selectNameFilter);
+  // const contacts = useSelector(selectContacts);
+  // const filter = useSelector(selectNameFilter);
+  const filteredContacts = useSelector(selectFilteredContacts);
 
-  const filteredContacts = contacts.filter(contact =>
-    contact.name.toLowerCase().includes(filter.toLowerCase())
-  );
+  // const filteredContacts = contacts.filter(contact =>
+  //   contact.name.toLowerCase().includes(filter.toLowerCase())
+  // );
 
   return (
     <ul ref={parent} className={css.list}>
